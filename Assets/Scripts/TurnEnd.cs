78522aed30105d579c911b6affec8fdfdf352a82
nagasakotsuyoshi <s153071@matsu.shimane-u.ac.jp>
2018-05-29 19:27:19 +0900

    セルの配置

---
+++ Assets/Scripts/TurnEnd.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnEnd : MonoBehaviour {
+
+	// Use this for initialization
+	public void turnend() {
+        if (transform.localEulerAngles.x <= 180)
+        {
+            transform.Rotate(new Vector3(180, 0, 0));
+        }
+        else
+        {
+            transform.Rotate(new Vector3(0, 0, 0));
+        }
+	}
+	
+
+}
