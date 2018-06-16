7dac2c884b149b587e57a7d8af4196f48b336348
nagasakotsuyoshi <s153071@matsu.shimane-u.ac.jp>
2018-06-17 08:28:27 +0900

    ドローする位置は完了

--- Assets/Scripts/Draw.cs
+++ Assets/Scripts/Draw.cs
@@ -6,9 +6,15 @@ public class Draw : MonoBehaviour {
 
     public void once_draw()
     {
-        GameObject obj = (GameObject)Resources.Load("Prefabs/Cell");
 
-        Instantiate(obj, this.GetComponent<RectTransform>().anchoredPosition, Quaternion.identity);
+        GridLayout grid = this.GetComponent<GridLayout>();
+        GameObject obj = (GameObject)Resources.Load("Prefabs/Cell");
+        GameObject prefab = (GameObject)Instantiate(obj);
+        prefab.transform.SetParent(this.transform);
+
+        //prefab.transform.localScale = new Vector3(50, 70, 0);
+        prefab.AddComponent<DragAndDropCell>();
+        //Instantiate(obj, this.GetComponent<RectTransform>().anchoredPosition, Quaternion.identity);
 
 
     }
