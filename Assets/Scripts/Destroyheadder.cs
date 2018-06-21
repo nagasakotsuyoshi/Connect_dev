0a5bb49edb7366248049be89676257683cfbfaf7
seigayokoyama <tomiyamakko@yahoo.co.jp>
2018-06-21 16:48:02 +0900

    ヘッダーの機能追加

---
+++ Assets/Scripts/Destroyheadder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyheadder : MonoBehaviour {
+    void Start()
+    {
+        Destroyheadder.DestoryAll();
+    }
+
+        static List<GameObject> dontDestroyObjects = new List<GameObject>();
+        static public void DontDestroyOnLoad(GameObject obj)
+        {
+            Object.DontDestroyOnLoad(obj);
+            dontDestroyObjects.Add(obj);
+        }
+
+        static public void DestoryAll()
+        {
+            foreach (var obj in dontDestroyObjects)
+            {
+                GameObject.Destroy(obj);
+            }
+            dontDestroyObjects.Clear();
+           
+        }
+    }
+
