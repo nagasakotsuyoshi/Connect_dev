583f8f4c18876f0da34e1ab464c95bae03437b22
nagasakotsuyoshi <naga2443b.o.c4649@gmail.com>
2018-06-23 02:32:41 +0900

    モーダルウィンドウまでできた

---
+++ Assets/Scripts/Destroyheadder.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyheadder : MonoBehaviour {
+    void Start()
+    {
+        Destroy(this.gameObject);
+        Destroyheadder.DestoryAll();
+    }
+
+        static List<GameObject> dontDestroyObjects = new List<GameObject>();
+        static public void DontDestroyOnLoad(GameObject obj)
+        {
+            Object.DontDestroyOnLoad(obj);
+            dontDestroyObjects.Add(obj);
+        }
+
+        static public void DestoryAll()
+        {
+            foreach (var obj in dontDestroyObjects)
+            {
+                GameObject.Destroy(obj);
+            }
+            dontDestroyObjects.Clear();
+           
+        }
+    }
+
