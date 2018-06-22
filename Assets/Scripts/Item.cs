ccb492cdb4929cc1d09fae7065338f560e61c09a
kzk-horie <kazukihorie.jp@gmail.com>
2018-06-23 08:19:55 +0900

    特定のCellでのDragAndDropの同期

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Item : NetworkBehaviour {
+
+    [SyncVar(hook = "OnParentNetIdChange")]
+    public NetworkInstanceId parentNetId;
+
+    void Start () {
+	}
+
+	void Update () {
+    }
+
+    public NetworkInstanceId GetNetId()
+    {
+        return this.GetComponent<NetworkIdentity>().netId;
+    }
+
+    [ClientCallback]
+    void OnParentNetIdChange(NetworkInstanceId newVal)
+    {
+        Debug.Log("Attached item net id:" + newVal);
+        NetworkInstanceId itemNetId = this.GetNetId();
+        GameObject targetItem = ClientScene.FindLocalObject(itemNetId);
+        GameObject parentCell = ClientScene.FindLocalObject(newVal);
+        targetItem.transform.SetParent(parentCell.transform, false);
+    }
+
+}
