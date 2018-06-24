df673bd62203844b3292a12a4820ca0240dbbeb2
kzk-horie <kazukihorie.jp@gmail.com>
2018-06-24 16:00:38 +0900

    player毎のカメラを設定

--- Assets/Scripts/Player2.cs
+++ Assets/Scripts/Player2.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class Player : NetworkBehaviour
+public class Player2 : NetworkBehaviour
 {
+    GameObject mainCamObj;
     public GameObject axe;
 
     void Start()
-    {}
+    {
+        if (isLocalPlayer)
+        {
+            mainCamObj = Camera.main.gameObject;
+            mainCamObj.transform.rotation = Quaternion.Euler(0, 0, 180);
+        }
+    }
 
     void Update()
     {
-        if (isLocalPlayer) { 
-            if (Input.GetKeyDown(KeyCode.Space)) {
+        if (isLocalPlayer)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
                 CmdSpawnAxe();
             }
         }
@@ -25,7 +34,7 @@ public class Player : NetworkBehaviour
         GameObject obj = Instantiate<GameObject>(axe, new Vector3(0, 0, 0), Quaternion.identity);
         Cell cell = GameObject.Find("BattleCell0").GetComponent<Cell>();
         NetworkInstanceId netId = cell.GetNetId();
-        GameObject targetCell = NetworkServer.FindLocalObject(netId);       
+        GameObject targetCell = NetworkServer.FindLocalObject(netId);
         NetworkServer.SpawnWithClientAuthority(obj, connectionToClient);
         obj.GetComponent<Transform>().transform.SetParent(targetCell.transform, false);
         obj.GetComponent<Item>().parentNetId = netId;
