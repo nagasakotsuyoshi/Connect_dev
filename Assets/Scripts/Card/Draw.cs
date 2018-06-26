02d5d8ee3b3012eb97c9f79a7dfcf3c30bcc21fd
nagasakotsuyoshi <naga2443b.o.c4649@gmail.com>
2018-06-26 09:17:40 +0900

    山札作成前

--- Assets/Scripts/Card/Draw.cs
+++ Assets/Scripts/Card/Draw.cs
@@ -6,20 +6,21 @@ public class Draw : MonoBehaviour {
 
     void Start()
     {
+        GameObject obj = null;
         for (int i = 0; i < 5; i++)
         {
-            once_draw();
-            
+            obj = once_draw();
+            obj.transform.position = new Vector3(obj.transform.position.x + i*10, obj.transform.position.y, obj.transform.position.z);
         }
     }
-    public void once_draw()
+    public GameObject once_draw()
     {
 
 
-        GameObject obj = (GameObject)Resources.Load("Prefabs/Cell");
+        GameObject obj = (GameObject)Resources.Load("Prefabs/Cell2d");
         GameObject prefab = (GameObject)Instantiate(obj);
         prefab.transform.SetParent(this.transform,false);
-
+        return prefab;
         //prefab.transform.localScale = new Vector3(50, 70, 0);
         //prefab.AddComponent<DragAndDropCell>();
         //Instantiate(obj, this.GetComponent<RectTransform>().anchoredPosition, Quaternion.identity);
