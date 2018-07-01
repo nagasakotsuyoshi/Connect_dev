8d6e74f7c0c16181ecbd16e0c2b271988cb5aebb
nagasakotsuyoshi <naga2443b.o.c4649@gmail.com>
2018-07-01 15:48:02 +0900

    GameController.csのひな型作成

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,12 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Networking;
 
 public class GameController : NetworkBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    //ターンのステータス
+    public enum Turn
+    {
+        Initializing,
+        Player1,
+        Player2,
+        Result,
+    }
+
+    //フェイズのステータス
+    //Trello のconnectボードの”もっと細分化”のリストの”ステータス”カードを参照
+    public enum Phase
+    {
+        Initializing, //初期値
+        Draw,    //カードを引く
+        UseCard, //カードを使う、置く
+        Attack,  //敵に攻撃する
+        Defend,  //敵の攻撃から守る
+        Decision,//勝敗の判定
+        End,     //ターンを終了
+        Waiting, //待つ
+    }
+
+    // プレイヤーの状態
+    [SyncVar(hook = "OnTurnChanged")]
+    public Turn m_Turn = Turn.Initializing;
+    [SyncVar(hook = "OnPhaseChanged")]
+    public Phase m_Phase = Phase.Initializing;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -14,4 +44,35 @@ public class GameController : NetworkBehaviour {
 	void Update () {
 		
 	}
+
+    // ターンの状態を変更する
+    [Server]
+    public void ChangeTurn(Turn turn)
+    {
+        // m_Phaseを変更すれば、あとはhookによりOnTurnChangedが実行される
+        m_Turn = turn;
+    }
+
+
+
+    void OnTurnChange(Turn turn)
+    {
+
+    }
+
+
+    // フェイズの状態を変更する
+    [Server]
+    public void ChangePhase(Phase phase)
+    {
+        // m_Phaseを変更すれば、あとはhookによりOnPhaseChangedが実行される
+        m_Phase = phase;
+    }
+
+
+
+    void OnPhaseChange(Phase phase)
+    {
+
+    }
 }
